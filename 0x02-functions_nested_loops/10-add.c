7e7db9a45e6a3eeeb003011ba82fba298f779d46
AnaliceBernice <analicebernice20@gmail.com>
2022-12-13 09:57:44 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Entry point for the addition of integers
+ * @a: first value to be added
+ * @b: second value to be added
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
