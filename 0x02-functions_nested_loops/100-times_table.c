8c6c01461a8ba86829569c6e624bbea689c7aca7
AnaliceBernice <analicebernice20@gmail.com>
2022-12-13 12:52:34 -0800

    Advanced Task 1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,7 +28,7 @@ void print_times_table(int n)
 				_putchar(' ');
 				if (product <= 9)
 				{
-					_putchar(',');
+					_putchar(' ');
 					_putchar(' ');
 					_putchar('0' + product);
 				}
@@ -42,7 +42,7 @@ void print_times_table(int n)
 				{
 
 					_putchar('0' + (product / 100));
-					_putchar('0' + ((product % 10) % 10));
+					_putchar('0' + ((product / 10) % 10));
 					_putchar('0' + (product % 10));
 				}
 			}
