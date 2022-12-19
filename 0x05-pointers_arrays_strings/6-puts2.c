79e26d64907ff009c2eee9d8a09446db4fcbac3d
AnaliceBernice <analicebernice20@gmail.com>
2022-12-19 06:45:52 -0800

    Task 6-9

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: pointer
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
