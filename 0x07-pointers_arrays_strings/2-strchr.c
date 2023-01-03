12b833e0d9ebd77bc090f9bcb425195438161a12
AnaliceBernice <analicebernice20@gmail.com>
2023-01-03 07:16:54 -0800

    Task 6-9

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,11 +2,12 @@
 #define NULL 0
 
 /**
- * strchr - locate 1st occurrence of char in string and returns pointer there
+ * _strchr - locate 1st occurrence of char in string and returns pointer there
  * @s: string to search
  * @c: target characer
  * Return: pointer to that character in string
  */
+
 char *_strchr(char *s, char c)
 {
 	int i = 0;
