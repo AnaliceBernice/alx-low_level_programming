b7eb020cdf3005d8e52124fa1dd97f3532e36270
AnaliceBernice <analicebernice20@gmail.com>
2023-01-09 06:38:42 -0800

    Task 0

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - function that concatenates two strings.
+ * at most n bytes from src
+ * @dest: pointer to destination string
+ * @src: pointer to source
+ * @n: pointer to number of byte to be concatenated
+ *
+ * Return: pointer to destination string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int length, j;
+
+	length = 0;
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+	for (j = 0; j < n && src[j] != '\0'; j++, length++)
+	{
+		dest[length] = src[j];
+	}
+	dest[length] = '\0';
+	return (dest);
+}
